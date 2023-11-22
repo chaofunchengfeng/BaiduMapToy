3074e06390263ad1025aba4ac8044858a79f2695
chaofunchengfeng <chaofunchengfeng@qq.com>
2023-11-23 07:01:49 +0800

    feat:查看腾讯地图街景

---
+++ js/content-script-jiejing.qq.com.js
@@ -0,0 +1,4 @@
+Object.defineProperty(navigator, 'userAgent', {
+    value: "Mozilla/5.0 (iPhone; CPU iPhone OS 17_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1.1 Mobile/15E148 Safari/604.1",
+    writable: false
+});
